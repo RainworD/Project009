db8a021a34c7dd942b157bd37d86c075842b9bb0
RainworD <673227139@qq.com>
2016-08-15 12:14:37 +0800

    左侧栏目切换实现
    为备份已改完，有些地方换了一种实现方式

--- qingta/source/js/public.js
+++ qingta/source/js/public.js
@@ -18,14 +18,6 @@ function init(){
 		var myhash=$(this).attr("href");
 		window.location.hash=myhash;
 	})
-	function loadContent() {
-	    var hash = window.location.hash;
-	    if (hash == "") {
-	        hash = "#/"+ADMIN_CONFIG.homePage;
-	    }
-	    $(ADMIN_CONFIG.contentSelector).load(hash.split("/")[1], function(){
-	    });
-	}
 }
 function eventBind(){
 	$(".downflag").bind("click",function(){
@@ -33,10 +25,34 @@ function eventBind(){
 		var array=showflag.split(" ");
 		if(array.length>1){
 			$(this).parent().next(".downContent").removeClass("show");
+			$(this).parent().removeClass("btnColor");
 		}
 		else{
 			$(this).parent().next(".downContent").addClass("show");
+			$(this).parent().addClass("btnColor");
 		}
 	})
+	$(".rightBox").on("click",".leftImg",function(){
+		var showflag1=$(this).parents(".rightBox").attr("class");
+		var array=showflag1.split(" ");
+		if(array.length>1){
+			$(".leftBox").removeClass("small");
+			$(".rightBox").removeClass("rightnew");
+			$(this).removeClass("showImg");
+		}
+		else{
+			$(".leftBox").addClass("small");
+			$(".rightBox").addClass("rightnew");
+			$(this).addClass("showImg");
+		}		
+	})
 	$(window).bind('load hashchange', loadContent);
+}
+function loadContent() {
+    var hash = window.location.hash;
+    if (hash == "") {
+        hash = "#/"+ADMIN_CONFIG.homePage;
+    }
+    $(ADMIN_CONFIG.contentSelector).load(hash.split("/")[1], function(){
+    });
 }
