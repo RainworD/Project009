2c05b7c2bed00df915f6762d2ab5f4e2fd66e4d2
RainworD <673227139@qq.com>
2016-08-26 17:35:05 +0800

    tb

--- qingta/source/js/public.js
+++ qingta/source/js/public.js
@@ -60,18 +60,72 @@ function eventBind(){
 			$(".rightBox").addClass("rightnew");
 		}		
 	})
-	// $(".small").delegate(".leftImg","click",function(){
-	// 	alert("111");
-	// 	$(this).addClass(".newLeftImg");
-	// 	$(this).parent(".section").siblings().children(".leftImg").removeClass(".newLeftImg");
-	// })
+	$(".changeBarBox").on("click",".changeButton",function(){
+		$(this).addClass("green").siblings().removeClass("green");
+	})
+	$(".arrowDown").bind("click",function(){
+		var className=$(this).siblings('.lookMore').attr("class");
+		var txt=$(this).parent("td").children(".txt").text();
+		var array=className.split(" ");
+		if(array.length>1){
+			$(this).siblings('.lookMore').removeClass("showMore");
+		}
+		else{
+			$(this).siblings('.lookMore').addClass("showMore");
+			$(this).siblings('.lookMore').children("span").text(txt);
+		}
+	})
 	$(window).bind('load hashchange', loadContent);
 }
 function loadContent() {
+	$(document).unbind("mousedown");
+	$(document).unbind("mouseover");
     var hash = window.location.hash;
     if (hash == "") {
         hash = "#/"+ADMIN_CONFIG.homePage;
     }
     $(ADMIN_CONFIG.contentSelector).load(hash.split("/")[1], function(){
     });
+}
+function popAlertBox(selector){//弹出窗口，居中function。以前在弹窗上绑定的一些事件
+	var height = $(window).height() - $(selector).height();
+	var width = $(window).width() - $(selector).width();
+	$(selector).css({
+		"top": height/2+"px",
+		"left":width/2+"px"
+	});
+	$(selector).show();
+
+}
+function dragBox(selector){//弹窗窗口的拖拽方法
+	$(document).unbind("mousemove,mouseup");
+	var myleft,mytop;
+	var ismousedown=false;
+	var downX,downY;
+	myleft=$(selector).position().left;
+	mytop=$(selector).position().top;
+	$(selector).bind("mousedown",function(e){
+		ismousedown=true;
+		myleft=$(selector).position().left;
+		mytop=$(selector).position().top;
+		downX=e.pageX;
+		downY=e.pageY;
+	})
+	$(document).bind("mousemove",function(e){
+			if(ismousedown){
+				console.log(e.pageX)
+				console.log(downX)
+				_mytop=e.pageY-downY + mytop;
+				_myleft=e.pageX-downX + myleft;
+				console.log("left2:"+myleft);
+				console.log("top2:"+mytop);
+				$(selector).css("left",_myleft);
+				$(selector).css("top",_mytop);
+			}
+	});
+	$(document).bind("mouseup",function(){
+		ismousedown=false;
+		mytop=$(selector).position().top;
+		myleft=$(selector).position().left;
+	})
 }
